574db8867e430231546720e8d92fd269881bff54
chihiro1010 <chihiroohba.info@gmail.com>
2023-12-10 02:30:05 +0900

    盤と駒の配置まで実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,132 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+interface State {
+  id: number;
+  state: string;
+}
 
 export default function Home() {
+  const [state, setState] = useState<Array<State>>([
+    { id: 1, state: "none" },
+    { id: 2, state: "none" },
+    { id: 3, state: "none" },
+    { id: 4, state: "none" },
+    { id: 5, state: "none" },
+    { id: 6, state: "none" },
+    { id: 7, state: "none" },
+    { id: 8, state: "none" },
+    { id: 9, state: "none" },
+    { id: 10, state: "none" },
+    { id: 11, state: "none" },
+    { id: 12, state: "none" },
+    { id: 13, state: "none" },
+    { id: 14, state: "none" },
+    { id: 15, state: "none" },
+    { id: 16, state: "none" },
+    { id: 17, state: "none" },
+    { id: 18, state: "none" },
+    { id: 19, state: "none" },
+    { id: 20, state: "none" },
+    { id: 21, state: "none" },
+    { id: 22, state: "none" },
+    { id: 23, state: "none" },
+    { id: 24, state: "none" },
+    { id: 25, state: "none" },
+    { id: 26, state: "none" },
+    { id: 27, state: "none" },
+    { id: 28, state: "none" },
+    { id: 29, state: "none" },
+    { id: 30, state: "none" },
+    { id: 31, state: "none" },
+    { id: 32, state: "none" },
+    { id: 33, state: "none" },
+    { id: 34, state: "none" },
+    { id: 35, state: "none" },
+    { id: 36, state: "none" },
+    { id: 37, state: "none" },
+    { id: 38, state: "none" },
+    { id: 39, state: "none" },
+    { id: 40, state: "none" },
+    { id: 41, state: "none" },
+    { id: 42, state: "none" },
+    { id: 43, state: "none" },
+    { id: 44, state: "none" },
+    { id: 45, state: "none" },
+    { id: 46, state: "none" },
+    { id: 47, state: "none" },
+    { id: 48, state: "none" },
+    { id: 49, state: "none" },
+    { id: 50, state: "none" },
+    { id: 51, state: "none" },
+    { id: 52, state: "none" },
+    { id: 53, state: "none" },
+    { id: 54, state: "none" },
+    { id: 55, state: "none" },
+    { id: 56, state: "none" },
+    { id: 57, state: "none" },
+    { id: 58, state: "none" },
+    { id: 59, state: "none" },
+    { id: 60, state: "none" },
+    { id: 61, state: "none" },
+    { id: 62, state: "none" },
+    { id: 63, state: "none" },
+    { id: 64, state: "none" },
+    { id: 65, state: "none" },
+    { id: 66, state: "none" },
+    { id: 67, state: "none" },
+    { id: 68, state: "none" },
+    { id: 69, state: "none" },
+    { id: 70, state: "none" },
+    { id: 71, state: "none" },
+    { id: 72, state: "none" },
+    { id: 73, state: "none" },
+    { id: 74, state: "none" },
+    { id: 75, state: "none" },
+    { id: 76, state: "none" },
+    { id: 77, state: "none" },
+    { id: 78, state: "none" },
+    { id: 79, state: "none" },
+    { id: 80, state: "none" },
+    { id: 81, state: "none" },
+  ]);
+  const [turn, setTrun] = useState<Boolean>(false);
+
+  const putPiece = (item: State): void => {
+    if (item.state === "none") {
+      setState((prev) => {
+        return [...prev].map((prevItem) => {
+          if (prevItem.id === item.id) {
+            return {
+              id: item.id,
+              state: turn ? "black" : "white",
+            };
+          } else {
+            return prevItem;
+          }
+        });
+      });
+      setTrun(!turn);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="bg-slate-800">
+      <div className="flex flex-wrap content-center w-96 m-auto justify-center  h-screen">
+        {state.map((item) => (
+          <button key={item.id} onClick={() => putPiece(item)}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src={"/" + item.state + ".svg"}
+              alt="黒"
+              width={40}
+              height={40}
               priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            ></Image>
+          </button>
+        ))}
       </div>
     </main>
-  )
+  );
 }
